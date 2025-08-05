730ee469a0b78e22b28e65aadf489dbfb604fe86
dovankhang <khangdv@blueoath.jp>
2025-08-05 18:24:57 +0700

    fix: use permissin to authorization

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,7 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { IS_PUBLIC_KEY } from 'src/decorator/customize';
 
 @Injectable()
@@ -21,10 +22,25 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         return super.canActivate(context);
     }
 
-    handleRequest(err, user, info) {
+    handleRequest(err, user, info, context: ExecutionContext) {
+        const request: Request = context.switchToHttp().getRequest();
+
         // You can throw an exception based on either "info" or "err" arguments
         if (err || !user) {
             throw err || new UnauthorizedException("Token không hợp lệ hoặc không có token ở Bearer Token ở Header request!");
+        }
+
+        //check permissions
+        const targetMethod = request.method;
+        const targetEndpoint = request.route?.path;
+
+        const permissions = user?.permissions ?? [];
+        const isExits = permissions.find(permission =>
+            targetMethod === permission.method && targetEndpoint === permission.apiPath
+        )
+
+        if (!isExits) {
+            throw new ForbiddenException("Bạn không có quyền truy cập endpoint này");
         }
         return user;
     }
--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -5,11 +5,13 @@ import { Injectable } from '@nestjs/common';
 import { jwtConstants } from '../constants';
 import { ConfigService } from '@nestjs/config';
 import { IUser } from 'src/users/users.interface';
+import { RolesService } from 'src/roles/roles.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
-        private configService: ConfigService
+        private configService: ConfigService,
+        private rolesService: RolesService
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -20,12 +22,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     async validate(payload: IUser) {
         const { _id, name, email, role } = payload;
+        // cần gán thêm permissions vào req.user
+
+        const userRole = role as unknown as { _id: string; name: string }
+        const temp = (await this.rolesService.findOne(userRole._id)).toObject()
+
         //req.user
         return {
             _id,
             name,
             email,
-            role
+            role,
+            permissions: temp?.permissions ?? []
         };
     }
 }
