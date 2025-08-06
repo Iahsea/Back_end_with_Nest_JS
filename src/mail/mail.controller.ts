b4f56e7edb91d29ca41aaedc5da13b6c36f773d4
dovankhang <khangdv@blueoath.jp>
2025-08-06 12:34:02 +0700

    fix(mail): send email with template

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -20,9 +20,11 @@ export class MailController {
       subject: 'Welcome to Nice App! Confirm your Email',
       template: "job",
       context: {
-        username: 'Nguyễn Văn A', // Truyền đúng tên biến như trong template
-        appName: 'Nice App',
-        confirmUrl: `https://yourapp.com/verify?email`,
+        username: 'Nguyễn Văn A',
+        appName: 'Recruitment Pro',
+        position: 'Backend Developer',
+        feedback: 'CV của bạn rất tốt, tuy nhiên chúng tôi cần thêm minh chứng về kinh nghiệm phát triển API với NestJS và MongoDB.',
+        viewCvUrl: 'https://yourapp.com/cv/view/12345',
         year: new Date().getFullYear()
       }
     });
